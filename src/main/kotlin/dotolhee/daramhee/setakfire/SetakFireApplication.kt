b6cc9b994f275acda0043966334ceaeb04a63c33
junnnyyy <junnnyyy@kakao.com>
2023-01-26 20:13:38 +0900

    project setting

---
+++ src/main/kotlin/dotolhee/daramhee/setakfire/SetakFireApplication.kt
@@ -0,0 +1,11 @@
+package dotolhee.daramhee.setakfire
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SetakFireApplication
+
+fun main(args: Array<String>) {
+	runApplication<SetakFireApplication>(*args)
+}
---
+++ src/test/kotlin/dotolhee/daramhee/setakfire/SetakFireApplicationTests.kt
@@ -0,0 +1,13 @@
+package dotolhee.daramhee.setakfire
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SetakFireApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
