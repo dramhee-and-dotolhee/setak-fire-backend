1a91381c2f8657e1e722888a15fc5c687ab9f851
junnnyyy <junnnyyy@kakao.com>
2023-07-23 22:32:42 +0900

    feat: Response변경

--- src/main/kotlin/dotolhee/daramhee/setakfire/user/controller/AuthenticationController.kt
+++ src/main/kotlin/dotolhee/daramhee/setakfire/user/controller/AuthenticationController.kt
@@ -1,7 +1,6 @@
 package dotolhee.daramhee.setakfire.user.controller
 
 import dotolhee.daramhee.setakfire.user.dto.LoginDTO
-import dotolhee.daramhee.setakfire.user.dto.LoginResponseDTO
 import dotolhee.daramhee.setakfire.user.dto.RegisterDTO
 import dotolhee.daramhee.setakfire.user.service.UserService
 import io.swagger.v3.oas.annotations.tags.Tag
@@ -35,7 +34,14 @@ class AuthenticationController(
     }
 
     @PostMapping("/login")
-    fun authenticate(@RequestBody request: LoginDTO): LoginResponseDTO {
-        return userService.login(request)
+    fun authenticate(@RequestBody request: LoginDTO):
+            ResponseEntity<out Any> {
+        return try {
+            val response = userService.login(request)
+            ResponseEntity(response, HttpStatus.OK)
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            ResponseEntity(ex.message, HttpStatus.BAD_REQUEST)
+        }
     }
 }
